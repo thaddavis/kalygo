7a31244481e54fed82a81653ad42d45fcf503eb1
Your Name <you@example.com>
2023-02-01 22:27:32 -0500

    hardcoding Blockchain Overview to Ethereum for Pitch

--- src/services/ethereum_client.ts
+++ src/services/ethereum_client.ts
@@ -24,7 +24,8 @@ export class EthereumClient {
       // );
 
       EthereumClient.ethereumClientInstance = new Web3(
-        "https://prettiest-withered-model.ethereum-goerli.discover.quiknode.pro/1e21d2d24cf75481f03311a5a652a4963e05ace4/"
+        // "https://prettiest-withered-model.ethereum-goerli.discover.quiknode.pro/1e21d2d24cf75481f03311a5a652a4963e05ace4/"
+        "https://virulent-smart-dream.discover.quiknode.pro/bf18a1026d8aa87ae439779802efb92be937e564/"
       );
     } catch (err: any) {
       console.log(err.stack);
